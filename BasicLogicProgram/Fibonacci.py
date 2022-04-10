0a1818184dcd46d9e8d04c0764545d2e919dfa58
SantoshThakare <santoshthakare50@gmail.com>
2022-04-10 21:41:25 +0530

    Basic Logical Program

--- BasicLogicProgram/Fibonacci.py
+++ BasicLogicProgram/Fibonacci.py
@@ -1,1 +1,11 @@
-a = int()
+a = int(input('Enter the 1st Number\n'))
+b = int(input('Enter the 2st Number\n'))
+n = int(input('enter the element of elements\n'))
+
+print(a, b, end=" ")
+while n-2:
+    c = a+b
+    a = b
+    b = c
+    print(c, end=" ")
+    n= n-1
